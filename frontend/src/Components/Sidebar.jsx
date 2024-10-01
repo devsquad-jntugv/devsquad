0b891d8c167919bf54431d476259ae695e94ed46
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-10-01 13:35:23 +0530

    initial

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
             </div>
         </div>
     </div>
-    <div className='z-[7] lg:hidden fixed right-0 top-[60px] sm:top-[70px] w-[100%] flex justify-end items-center gap-2 px-2 py-2 mb-5 shdow-sm shadow-slate-400  bg-white'>
+    <div className='z-[7] lg:hidden fixed right-0 top-[60px] sm:top-[70px] w-[100%] flex justify-end items-center gap-2 px-2 py-2 mb-5 shdow-sm shadow-slate-400  bg-slate-400'>
     <div className='lg:hidden mb-2'>
         <select className='py-2 px-2 outline-none border-none bg-white text-[16px] font-medium' value={selectedPage} onChange={handelNavInMobile}>
             <option className='py-2 px-2' value='/dashboard'>Main Dashboard</option>
