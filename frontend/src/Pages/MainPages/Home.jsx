0a565071092a2c423ba25c553e624901f4ff946f
Varun Kumar <varunkumarsunkaraboina@gmail.com>
2024-10-01 13:50:24 +0530

    initial

--- frontend/src/Pages/MainPages/Home.jsx
+++ frontend/src/Pages/MainPages/Home.jsx
@@ -157,7 +157,7 @@ workshops, this prepares students for the fast evolving world of media and commu
       </section>
       <section className="w-[100%] py-16 px-8">
         <div className="flex flex-col gap-3 justify-center items-center">
-          <h1 className="text-2xl sm:text-3xl lg:text-4xl text-purple-600 font-bold">
+          <h1 className="text-xl sm:text-2xl lg:text-3xl text-purple-600 font-bold">
             OUR DEPARTMENT COLLABORATIONS
           </h1>
           <div className="w-[170px] sm:w-[200px] h-[3px] rounded-sm bg-slate-400"></div>
